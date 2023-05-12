aa62afa01c2e5aabd66458b66d8fda039de275db
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-05-12 10:50:50 -0300

    Update ExcluirExpiradoApi.sh

--- ExcluirExpiradoApi.sh
+++ ExcluirExpiradoApi.sh
@@ -1,26 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
-USER_LIST="$1"
+# Verifica se o script está sendo executado como root
+if [[ $EUID -ne 0 ]]; then
+   echo "Este script deve ser executado como root" 
+   exit 1
+fi
+
+# Verifica se o argumento foi fornecido
+if [ -z "$1" ]; then
+    echo "Por favor, forneça uma lista de usuários para remover"
+    exit 1
+fi
 
 # Verifica a arquitetura do servidor
 if [ "$(uname -m)" = "aarch64" ]; then
   # Servidor ARM
-  PKILL_CMD="pkill"
-  USERDEL_CMD="userdel"
+  DELUSER_CMD="deluser"
 else
   # Servidor não-ARM
-  PKILL_CMD="/usr/bin/pkill"
-  USERDEL_CMD="/usr/sbin/userdel"
+  DELUSER_CMD="userdel"
 fi
 
 # Itera sobre a lista de usuários
-for USER in $USER_LIST; do
+while read -r USER; do
   # Encerra todos os processos do usuário
-  $PKILL_CMD -u $USER
+  pkill -u $USER
 
   # Remove o diretório home do usuário
-  rm -rf $HOME/$USER
+  rm -rf /home/$USER
 
   # Remove o usuário e seu diretório home
-  $USERDEL_CMD -r $USER
-done
+  $DELUSER_CMD --remove-home $USER
+done < "$1"
+
+echo "Usuários removidos com sucesso"
+
+
+
+
+
+
+
+
+
