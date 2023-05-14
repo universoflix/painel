e8979dfde33799c3284ea9fe6c57e43c26982319
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-05-13 23:24:52 -0300

    ExcluirExpiradoApi.sh

--- ExcluirExpiradoApi.sh
+++ ExcluirExpiradoApi.sh
@@ -6,11 +6,9 @@ USER_LIST="$1"
 if [ "$(uname -m)" = "aarch64" ]; then
   # Servidor ARM
   PKILL_CMD="pkill"
-  USERDEL_CMD="userdel"
 else
   # Servidor não-ARM
   PKILL_CMD="/usr/bin/pkill"
-  USERDEL_CMD="/usr/sbin/userdel"
 fi
 
 # Itera sobre a lista de usuários
@@ -18,9 +16,6 @@ for USER in $USER_LIST; do
   # Encerra todos os processos do usuário
   $PKILL_CMD -u $USER
 
-  # Remove o diretório home do usuário
-  rm -rf $HOME/$USER
-
-  # Remove o usuário e seu diretório home
-  $USERDEL_CMD -r $USER
+  # Remove o usuário
+  userdel $USER
 done
